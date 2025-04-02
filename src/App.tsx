d359acc515499ef9fcd5f57ad3f41b1611bb1020
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-04-01 23:56:58 -0300

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
-import { Header } from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
 
 export function App() {
 
   return (
     
     <ThemeProvider theme={defaultTheme}>
-      <Header />
-      <h1>Coffee Delivery</h1>
+      <BrowserRouter >
+        <Router/>
+      </BrowserRouter>
       <GlobalStyle/>
     </ThemeProvider>
     
