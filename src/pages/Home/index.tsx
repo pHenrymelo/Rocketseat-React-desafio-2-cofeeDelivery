13496e4c2942e2d0e4b6b152b37e4f7593f0e2d2
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-04-04 19:37:10 -0300

    feat: home banner structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,39 @@
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import cover from '../../assets/cover.svg'
+
 export function Home() {
     return(
         <div>
-            <h1>Home</h1>
+            <div>
+                <div>
+                    <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+                    <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+                    <div>
+                        <span>
+                            <ShoppingCart size={24} weight='fill'/>
+                            <p>Compra simples e segura</p>
+                        </span>
+                        <span>
+                            <Package size={24} weight='fill' />
+                            <p>Embalagem mantem o café intacto</p>
+                        </span>
+                        <span>
+                            <Timer size={24} weight='fill'/>
+                            <p>Entrega rápida e rastreada</p>
+                        </span>
+                        <span>
+                            <Coffee size={24} weight='fill' />
+                            <p>O café chega fresquinho até você</p>
+                        </span>
+                    </div>
+                </div>
+                <aside>
+                    <img src={cover} alt="copo de café ao centro com grãos e po de café em um fundo amarelo" />
+                </aside>
+            </div>
+            <main>
+                CAFES
+            </main>
         </div>
     )
 }
